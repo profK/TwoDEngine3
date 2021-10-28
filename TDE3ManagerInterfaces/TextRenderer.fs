921c3a03bbde564338810586138a394529b655ba
Jeff Kesselman <jpkessel@purdue.edu>
2021-10-27 21:15:19 -0400

    strated working on anglecode

---
+++ TDE3ManagerInterfaces/TextRenderer.fs
@@ -0,0 +1,17 @@
+﻿module TDE3ManagerInterfaces.TextRendererInterfaces
+
+open System.Numerics
+
+type Font =
+    abstract member Name : string
+    abstract member Size : int
+    abstract member MakeText : string -> Text
+    
+and Text =
+    abstract member Text : string
+    abstract member Font : Font
+       
+and TextManager =
+    abstract member FontList : Font list with get
+    abstract member RenderText: Vector2->Text->unit
+
