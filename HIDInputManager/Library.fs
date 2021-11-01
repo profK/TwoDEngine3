439fb03ec5c60329b871b6505fff99902184279e
professorK <jeffpk@gmail.com>
2021-11-01 19:53:52 -0400

    rewrittne for the thrid time, think the scannign is right now....

--- HIDInputManager/Library.fs
+++ HIDInputManager/Library.fs
@@ -1,44 +1,33 @@
 ﻿module HIDInputManager
 
+open System
 open System.Reflection.Metadata
 open DevDecoder.HIDDevices
+open DevDecoder.HIDDevices.Controllers
 open DynamicData
 open ManagerRegistry
 open TwoDEngine3.ManagerInterfaces.InputManager
 
 
-type HIDAxisNode(name, value,parent) =
-    inherit Node(name, value,parent)
+type AxisNode(ctrl:ControlValue,parent) =
+    inherit Node(ctrl.PropertyName,
+                 if ctrl.Type = typeof<bool> then
+                    Axis(Digital(false))
+                 else
+                    Axis(Analog(float 0))
+                 ,Some(parent))
 
-type HIDControlNode(control: Control,parent:Node) as this =
-    inherit Node(control.Name,
+
+
+type ControllerNode(controller: Controller,parent) as this =
+    inherit Node(controller.Name,
                  Children(
-                     { 0 .. (control.ElementCount - 1) }
-                     |> Seq.map
-                         (fun index ->
-                             HIDAxisNode(
-                                 index.ToString(),
-                                 match control.IsBoolean with
-                                 | true -> Axis(Digital(false))
-                                 | false -> Axis(Analog(float 0))
-                                 , Some(this :> Node)
-                             )
-                             :> Node)
+                     controller
+                     |> Seq.cast<ControlValue>
+                     |> Seq.map (fun ctrl -> (AxisNode(ctrl,this) :> Node))
                      |> Seq.toList
-                 ) ,Some(parent) )
+                 ), parent)
 
-type HIDDeviceNode(device: Device) as this =
-    inherit Node(device.Name,
-                 Children(
-                     device.Controls
-                     |> Seq.map (fun ctrl -> (HIDControlNode(ctrl,this) :> Node))
-                     |> Seq.toList
-                 ), None)
-
-    member val Device = device
-    
-
-    member val listenTo = false with get, set
 
 type ControlChange(devNode,ctrlNode,oldVal,newVal,tstamp) =
         member val DeviceNode = devNode with get
@@ -50,38 +39,18 @@ type ControlChange(devNode,ctrlNode,oldVal,newVal,tstamp) =
 [<Manager("A cross platform HID input manager", supportedSystems.Windows)>]
 type HIDInputManager() =
     let root = Node("root", Children(List.Empty), None)
-    let devices = new Devices()
+    let subsciption = (new Devices()).Controllers<Controller>().Subscribe(fun controller ->
+            root.Value <- Children(
+                match root.Value with
+                | Children list ->
+                    (ControllerNode(controller,Some(root)):>Node)::list
+                | _ -> failwith "Error: root does not have a children list as its value!"
+            )
+        )
+    
+ 
 
-    let hidDevices =
-        devices
-            .Connected()
-            .Subscribe(fun changeSet ->
-                changeSet
-                |> Seq.iter
-                    (fun action ->
-                        match action.Reason with
-                        | ChangeReason.Add ->
-                            root.Value <-
-                                Children(
-                                    (HIDDeviceNode(action.Current) :> Node)
-                                    :: match root.Value with
-                                       | Children oldlist -> oldlist
-                                       | _ -> List.Empty
-                                )
-
-                            printfn $"Added %s{action.Current.Name}"
-                        | ChangeReason.Remove ->
-                            root.Value <-
-                                Children(
-                                    match root.Value with
-                                    | Children oldlist ->
-                                        oldlist
-                                        |> List.filter (fun childNode -> childNode.Name = action.Current.Name)
-                                    | _ -> List.empty
-                                )
-
-                            printfn $"Removed %s{action.Key}"
-                        | _ -> printfn $"Unimplemented action: %s{action.Reason.ToString()}"))
+    
     
         
     let mutable changeList:ControlChange list = List.empty
