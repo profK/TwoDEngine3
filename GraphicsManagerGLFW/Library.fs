edc595d1cb54ed8bb9d4ca60d9a4ec107fedaf06
professorK <jeffpk@gmail.com>
2021-11-11 15:27:27 -0500

    render loop written

--- GraphicsManagerGLFW/Library.fs
+++ GraphicsManagerGLFW/Library.fs
@@ -1,5 +1,6 @@
 ﻿module GraphicsManagerGLFW
 
+open System
 open System.Numerics
 open System.Threading
 open TwoDEngine3.ManagerInterfaces.GraphicsManagerInterface
@@ -12,7 +13,7 @@ type OglXform() =
         member this.Multiply (other:Transform):Transform = failwith "todo"
         member this.Multiply (vec:Vector2): Vector2 = failwith "todo"
 
-type GraphicsManagerGLFW() =
+type GraphicsManagerGLFW()as this=
     //Create a window with the oop binding
     let vertShaderCode = [|
         "#version 400\n"
@@ -28,29 +29,10 @@ type GraphicsManagerGLFW() =
         "    frag_colour = vec4(0.5, 0.0, 0.5, 1.0);\n"
         "}"
     |]
-
-    
-    let init =
-        Glfw.Init() |> ignore
-        let vshader = Gl.CreateShader(ShaderType.VertexShader)
-        Gl.ShaderSource(vshader,vertShaderCode)
-        Gl.CompileShader(vshader)
-        let fshader =Gl.CreateShader(ShaderType.FragmentShader)
-        Gl.ShaderSource(fshader,fragShaderCode)
-        Gl.CompileShader(fshader)
-        use window = new Window(800, 600, "Glfw test window")
-        Glfw.SetWindowCloseCallback(window,fun args -> window.Close()) |> ignore
-        //You need to be notified if the size changed? 
-        //Simply add a handler to the Size changed event
-        window.SizeChanged.Add (fun args -> printfn "Size changed! New width %A, new height %A" args.width args.height)
-        while (not (window.ShouldClose())) do
-            Thread.Sleep(500)
-    do init
-  
-    
+     
     interface GraphicsManager with
         member this.DrawImage(var0) (var1) = failwith "todo"
-        member val GraphicsListeners = List.Empty  with get, set
+        member val GraphicsListeners:(GraphicsListener list) = List.Empty  with get, set
         member val IdentityTransform = failwith "todo"
         member this.LoadImage(var0) = failwith "todo"
         member this.PopClip() = failwith "todo"
@@ -59,7 +41,38 @@ type GraphicsManagerGLFW() =
         member this.PushTransform(var0) = failwith "todo"
         member this.RotationTransform(var0) = failwith "todo"
         member this.ScreenSize = failwith "todo"
-        member this.Start() = failwith "todo"
+        member this.Start() =
+            Glfw.Init() |> ignore
+            let vshader = Gl.CreateShader(ShaderType.VertexShader)
+            Gl.ShaderSource(vshader,vertShaderCode)
+            Gl.CompileShader(vshader)
+            let fshader =Gl.CreateShader(ShaderType.FragmentShader)
+            Gl.ShaderSource(fshader,fragShaderCode)
+            Gl.CompileShader(fshader)
+            let shaderProgram = Gl.CreateProgram()
+            Gl.AttachShader(shaderProgram,fshader)
+            Gl.AttachShader(shaderProgram,vshader)
+            Gl.LinkProgram(shaderProgram)
+            use window = new Window(800, 600, "Glfw test window")
+            Glfw.SetWindowCloseCallback(window,fun args -> window.Close()) |> ignore
+            //You need to be notified if the size changed? 
+            //Simply add a handler to the Size changed event
+            window.SizeChanged.Add (fun args -> printfn "Size changed! New width %A, new height %A" args.width args.height)
+            while 
+                (this :> GraphicsManager).GraphicsListeners 
+                |> List.tryPick (fun listener -> listener.Update DateTime.Now.Millisecond)
+                |> function
+                   | Some (error) ->
+                        printfn $"Quitting because %s{error}"
+                        false
+                   | None -> true
+                do
+                    Gl.Clear(ClearBufferMask.ColorBufferBit|||ClearBufferMask.DepthBufferBit)
+                    Gl.UseProgram(shaderProgram)
+                    (this :> GraphicsManager).GraphicsListeners 
+                    |> Seq.iter(fun listener-> listener.Render(this) )
+                    Glfw.SwapBuffers(window)
+                    Glfw.PollEvents();
         member this.Start(var0) = failwith "todo"
         member this.TranslationTransform(var0) = failwith "todo"
        
