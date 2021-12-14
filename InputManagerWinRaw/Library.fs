a6877f12d6b988f53c86954d22d05e8b8d8dcf5e
professorK <jeffpk@gmail.com>
2021-12-14 17:14:27 -0500

    puttign back to change to home computer....

--- InputManagerWinRaw/Library.fs
+++ InputManagerWinRaw/Library.fs
@@ -16,39 +16,15 @@ type InputManagerWinRaw() as this=
         
     let thread  = Thread(ThreadStart(inputThread)).Start()
     
+      
     member val RawInputOpt:RawInput option = None with get,set   
         
     interface InputDeviceInterface with
         
-        member val Controllers =
-            let stringCompare (str1:string, str2:string) =
-                if (str1.Length<str2.Length) then
-                    -1
-                else if (str1.Length>str2.Length) then
-                    +1
-                else
-                    [0..str1.Length-1]
-                    |> List.tryFindIndex(fun idx ->
-                        not (str1.Chars(idx) = str2.Chars(idx))
-                        )
-                    |> function
-                        | None -> 0
-                        | Some idx ->
-                            let char1 = str1.Chars(idx)
-                            let char2 = str2.Chars(idx)
-                            if (char1<char2) then
-                                -1
-                            else if (char2>char1) then
-                                 1
-                            else
-                                 0
-                                
+        member val Controllers =                
             NativeAPI.GetDevices()
-            |> Seq.sortWith(fun dev1 dev2 ->
-                    stringCompare(dev1.Names.Product, dev2.Names.Product)  
+            |> Seq.map(fun dev ->
                 )
-               
-            |> Seq.map
         member val StateChanges = failwith "todo"
         
         
