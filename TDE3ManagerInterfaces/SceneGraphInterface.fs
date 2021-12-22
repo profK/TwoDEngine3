6a92b9500f97f824abf3065a88508cc7f5f3ab09
Jeff Kesselman <jpkessel@purdue.edu>
2021-12-22 15:04:47 -0500

    Initial abstract graph created

---
+++ TDE3ManagerInterfaces/SceneGraphInterface.fs
@@ -0,0 +1,26 @@
+﻿module TDE3ManagerInterfaces.SceneGraphInterface
+
+open TDE3ManagerInterfaces
+type SceneGraphObjectContainerInterface =
+    abstract AddObject: SceneGraphObjectInterface -> SceneGraphObjectContainerInterface
+    abstract RemoveByPath: string -> SceneGraphObjectContainerInterface
+    abstract RemoveByPath: string list -> SceneGraphObjectContainerInterface
+    abstract RemoveObject: SceneGraphObjectInterface -> SceneGraphObjectContainerInterface
+    abstract Children : SceneGraphObjectInterface list
+
+
+and SceneGraphObjectInterface =
+    inherit SceneGraphObjectContainerInterface
+    abstract Name:string
+    abstract Path:string
+    abstract Parent:SceneGraphObjectContainerInterface
+    
+and SceneGraphInterface =
+    inherit SceneGraphObjectContainerInterface
+    abstract Update: uint -> string option
+    abstract Render: unit -> unit
+    
+    
+    
+    
+      
