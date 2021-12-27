614683fd450aecfbb5e2873c0de8f8082bb0cf76
Jeff Kesselman <jpkessel@purdue.edu>
2021-12-27 15:31:09 -0500

    Asteroids level getting lose to displayign ship

--- TwoDEngine3/AsteroidsLevel.fs
+++ TwoDEngine3/AsteroidsLevel.fs
@@ -1,19 +1,30 @@
 ﻿module TwoDEngine3.AsteroidsLevel
 
-
+open System.IO
+open TDE3ManagerInterfaces.SceneGraphInterface
 open TwoDEngine3.LevelManagerInterface
 open TwoDEngine3.ManagerInterfaces.GraphicsManagerInterface
+open TwoDEngine3.SceneGraphNodes
 
 type AsteroidsLevel() =
      inherit AbstractLevelController()
      let mutable ship = None
-     let sceneGraph = ManagerRegistry.getManager<SceneGraph>
+     let sceneGraph = ManagerRegistry.getManager<SceneGraphInterface>()
+     
      
      override this.Open() =
         base.Open()
-        let atlas = this.graphics.Value.LoadImage "asteriods-arcade.png"
-        let shipImage = atlas.SubImage (Rectangle (Vector 3 2) (Vector 25 30 ))
-        ship <- s
-        sceneGraph.AddSprite(ship)
+        use filestream = File.Open("Assets/asteriods-arcade.png", FileMode.Open)
+        let atlas = this.graphics.Value.LoadImage filestream
+                        
+        let shipImage = atlas.SubImage (
+                            Rectangle(
+                                this.graphics.Value.NewVector 3f 2f,
+                                this.graphics.Value.NewVector 25f 30f)
+                         )
+        let shipXform = this.graphics.Value.TranslationTransform 50f 50f 
+        let shipSprite = Sprite(shipImage,shipXform,
+                                this.graphics.Value.NewVector 0f 0f)
+        sceneGraph.Value.AddChild(shipSprite)
         
         
