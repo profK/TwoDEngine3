477ab88e7af5e647203747e685126a2fe9e77893
professorK <jeffpk@gmail.com>
2023-12-20 18:10:07 -0500

    initial commit

--- SceneGraph2D/Library.fs
+++ SceneGraph2D/Library.fs
@@ -4,49 +4,5 @@ open TDE3ManagerInterfaces.SceneGraphInterface
 open Tree
 open TwoDEngine3.ManagerInterfaces.GraphicsManagerInterface
 
-type private TreeNode = Tree<SceneGraphObjectInterface,SceneGraphObjectInterface>      
-type SG2DSceneGraph() as this =
-    let mutable tree= ()
-    do ManagerRegistry.getManager<GraphicsManager>()
-       |> function
-           | Some gm ->
-               gm.GraphicsListeners <- this::gm.GraphicsListeners
-           | None ->
-               printfn("Error: SG2DSceneGraph failed to find graphics manager")
-   
-    interface SceneGraphInterface with
-        member this.AddChild (child) : SceneGraphInterface=
-            tree.AddChild (TreeNode (Some child)) |> ignore
-            this
-        member this.RemoveChild (child) : SceneGraphInterface =
-            tree.FindChild (Some child)
-            |> function
-                | Some node ->
-                    tree.RemoveChild node |> ignore
-                | None -> ()
-            this
+      
 
-    
-        
-    interface GraphicsListener with
-        member this.Render(graphics) =
-            Tree.iter (fun parent child  ->
-                    match child.Data with
-                    | Some (data:SceneGraphObjectInterface) -> data.Render graphics
-                    | None -> ()
-                ) tree
-        member this.Update graphics deltaT =
-            tree <-
-              tree
-              |> Tree.fold (fun state parentNode childNode  ->
-                     let newData=
-                        (match childNode.Data with
-                            | Some data ->
-                                data.Update graphics deltaT 
-                            | None ->
-                                None
-                        )
-                     state.Children <- TreeNode(newData)::state.Children
-                     state
-                ) Tree.Empty 
-            None // no error interrupting right now, probably needs to be a fold
--- TDE3ManagerInterfaces/SceneGraphInterface.fs
+++ TDE3ManagerInterfaces/SceneGraphInterface.fs
@@ -1,18 +1,41 @@
 ﻿module TDE3ManagerInterfaces.SceneGraphInterface
-open TDE3ManagerInterfaces
+
+open System
 open TwoDEngine3.ManagerInterfaces.GraphicsManagerInterface
-
-type SceneGraphObjectInterface =
-    abstract Update: GraphicsManager->uint->SceneGraphObjectInterface option
+(***************************************************************
+This file is special in that it does not act as a front end to 
+pluggable back end code.  Rather it defines a scenegraph DSl used
+directly by the game application.
+It is in the Interfaces namespace for convenience
+****************************************************************)
+type SceneGraphObject([<ParamArray>] childlist: SceneGraphObject array) =
+    let initialChildList = childlist
+    abstract Update: GraphicsManager->uint->SceneGraphObject option
+    default this.Update gm deltaMs = Some this
     abstract Render: GraphicsManager->unit
-   
-
-type SceneGraphInterface =
-    abstract  AddChild: SceneGraphObjectInterface -> SceneGraphInterface
-    abstract  RemoveChild: SceneGraphObjectInterface -> SceneGraphInterface
+    default this.Render gm = ()
+    member val Xform:Transform = Transform.Identity with get, set
+    member val Children=initialChildList with get,set
     
-       
-   
+type Sprite( img:Image,  [<ParamArray>] childlist: SceneGraphObject array) = 
+    inherit SceneGraphObject(childlist)
+    let image = img
+    
+    override this.Render gm  =
+        match gm.PopTransform() with
+        | Some pt -> 
+                gm.PushTransform (base.Xform.Multiply pt)
+        | None -> gm.PushTransform (base.Xform)
+        gm.DrawImage image
+        Array.iter (
+            fun (child:SceneGraphObject) ->
+                child.Render gm
+            ) base.Children
+        gm.PopTransform() |> ignore
+        ()
+    
+    
+        
  
     
    
