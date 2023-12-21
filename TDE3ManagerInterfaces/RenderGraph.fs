bc65905a623486c7716844abd58d4b4b29dc165f
professorK <jeffpk@gmail.com>
2023-12-21 04:01:22 -0500

    workign on rendergraph and Graphics manager

---
+++ TDE3ManagerInterfaces/RenderGraph.fs
@@ -0,0 +1,37 @@
+﻿module TwoDEngine3.RenderGraphInterface
+
+open System
+open TwoDEngine3.ManagerInterfaces.GraphicsManagerInterface
+
+
+type RenderNode([<ParamArray>] childrenArray:RenderNode array ) =
+    let Children = childrenArray
+    
+    abstract member Render: Transform-> GraphicsManager -> unit
+    default this.Render xform gm =
+        this.RenderChildren xform gm
+    member this.RenderChildren xform gm =    
+        Children
+        |> Array.iter (fun child -> child.Render xform gm)
+
+    
+type Sprite(img:Image, [<ParamArray>] childrenArray:RenderNode array) =
+    inherit RenderNode(childrenArray)
+    let Image = img
+    override this.Render xform gm =
+        gm.DrawImage img
+        this.RenderChildren xform gm
+       
+type GenericTransform(transform:Transform, [<ParamArray>] childrenArray:RenderNode array) =
+    inherit RenderNode(childrenArray)
+    let myXform = transform
+    override this.Render xform gm =
+        let newXform = xform.Multiply myXform
+        this.RenderChildren newXform gm        
+        
+type Rotate(degrees:float, [<ParamArray>] childrenArray:RenderNode array) =
+    inherit GenericTransform(gm.Rot,  childrenArray)    
+    
+        
+    
+
